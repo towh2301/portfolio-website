87aa3377a054ea605f25d81ed43ab1e7c298c9a9
towh2301 <huy.buihoang2301@gmail.com>
2025-08-25 17:00:49 +0700

    update

--- src/app/pages/contact.tsx
+++ src/app/pages/contact.tsx
@@ -3,44 +3,44 @@ import React from "react";
 const sora = Sora({ subsets: ["latin"] });
 
 export default function Contact() {
-    return (
-        <div className="w-full gap-5 px-8 pt-14">
-            <div className="mb-14 pt-5 pb-2 w-full font-sora text-3xl md:text-5xl text-center font-['Sora'] items-center">
-                My <span className="text-black font-extrabold">Contact</span>
-            </div>
-            <div className="w-full flex-row md:flex gap-10 items-center justify-center">
-                <div className="justify-center items-center md:inline-flex grid grid-col-1 gap-24">
-                    <div className="self-stretch py-5 flex-col justify-center items-center">
-                        <form className="w-max h-96 flex-col justify-start items-start gap-5 flex">
-                            <div className="w-full flex-col justify-start items-start gap-3 flex">
-                                <input
-                                    placeholder="Your name..."
-                                    className="w-full md:w-96 md:h-14 px-6 py-4 rounded border border-black justify-start items-center inline-flex"
-                                ></input>
-                            </div>
-                            <div className="w-full flex-col justify-start items-start gap-3 flex">
-                                <input
-                                    placeholder="Your email..."
-                                    className="w-full md:w-96 md:h-14 px-6 py-4 rounded border border-black justify-start items-center inline-flex"
-                                ></input>
-                            </div>
-                            <div className="w-full flex-col justify-start items-start gap-3 flex">
-                                <input
-                                    placeholder="Your website(If exists)..."
-                                    className="w-full md:w-96 md:h-14 px-6 py-4 rounded border border-black justify-start items-center inline-flex"
-                                ></input>
-                            </div>
-                            <div className="w-full flex-col justify-start items-start gap-3 flex">
-                                <input
-                                    placeholder="How can I help?"
-                                    className="w-full md:w-96 md:h-36 px-6 py-4 rounded border border-black justify-start items-start inline-flex"
-                                ></input>
-                            </div>
-                            <div className="justify-start items-start gap-6 inline-flex">
-                                <button className="h-14 relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 px-5 w-[136px]">
-                                    Get in Touch
-                                </button>
-                                {/* <div className="w-14 h-14 p-4 rounded border-2 border-black justify-center items-center gap-2 flex">
+	return (
+		<div className="w-full gap-5 px-8 pt-14">
+			<div className="mb-14 pt-5 pb-2 w-full font-sora text-3xl md:text-5xl text-center font-['Sora'] items-center">
+				My <span className="text-black font-extrabold">Contact</span>
+			</div>
+			<div className="w-full flex-row md:flex gap-10 items-center justify-center">
+				<div className="justify-center items-center md:inline-flex grid grid-col-1 gap-24">
+					<div className="self-stretch py-5 flex-col justify-center items-center">
+						<form className="w-max h-96 flex-col justify-start items-start gap-5 flex">
+							<div className="w-full flex-col justify-start items-start gap-3 flex">
+								<input
+									placeholder="Your name..."
+									className="w-full md:w-96 md:h-14 px-6 py-4 rounded border border-black justify-start items-center inline-flex"
+								></input>
+							</div>
+							<div className="w-full flex-col justify-start items-start gap-3 flex">
+								<input
+									placeholder="Your email..."
+									className="w-full md:w-96 md:h-14 px-6 py-4 rounded border border-black justify-start items-center inline-flex"
+								></input>
+							</div>
+							<div className="w-full flex-col justify-start items-start gap-3 flex">
+								<input
+									placeholder="Your website(If exists)..."
+									className="w-full md:w-96 md:h-14 px-6 py-4 rounded border border-black justify-start items-center inline-flex"
+								></input>
+							</div>
+							<div className="w-full flex-col justify-start items-start gap-3 flex">
+								<input
+									placeholder="How can I help?"
+									className="w-full md:w-96 md:h-36 px-6 py-4 rounded border border-black justify-start items-start inline-flex"
+								></input>
+							</div>
+							<div className="justify-start items-start gap-6 inline-flex">
+								<button className="h-14 relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 px-5 w-[136px]">
+									Get in Touch
+								</button>
+								{/* <div className="w-14 h-14 p-4 rounded border-2 border-black justify-center items-center gap-2 flex">
                                     <div className="w-5 h-5 justify-center items-center flex">
                                         <div className="w-5 h-5 relative flex-col justify-start items-start flex">
                                             <img
@@ -62,38 +62,38 @@ export default function Contact() {
                                         </div>
                                     </div>
                                 </div> */}
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <div className="w-full py-5 flex-col justify-start gap-6 flex items-right text-justify">
-                    <div className="text-black text-3xl md:text-4xl font-extrabold font-['sora'] leading-10">
-                        Let’s talk for
-                    </div>
-                    <div className="self-stretch text-black text-4xl md:text-5xl font-extrabold font-['sora'] leading-10">
-                        Something special
-                    </div>
-                    <div className="self-stretch text-zinc-500 text-base font-normal font-['sora'] leading-normal tracking-tight">
-                        I seek to push the limits of creativity to create
-                        high-engaging, user-friendly, and memorable interactive
-                        experiences. I am always eager to learn new technologies
-                        and frameworks. I am passionate about building excellent
-                        software that improves the lives of those around me.
-                        That is why I am always looking for new opportunities to
-                        work with people who share the same passion and vision
-                        as me.
-                    </div>
-                    <div className="self-stretch text-black text-sm md:text-3xl font-semibold font-['sora']">
-                        huy.buihoang2301@gmail.com
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+							</div>
+						</form>
+					</div>
+				</div>
+				<div className="w-full py-5 flex-col justify-start gap-6 flex items-right text-justify">
+					<div className="text-black text-3xl md:text-4xl font-extrabold font-['sora'] leading-10">
+						Let’s talk for
+					</div>
+					<div className="self-stretch text-black text-4xl md:text-5xl font-extrabold font-['sora'] leading-10">
+						Something special
+					</div>
+					<div className="self-stretch text-zinc-500 text-base font-normal font-['sora'] leading-normal tracking-tight">
+						I seek to push the limits of creativity to create
+						high-engaging, user-friendly, and memorable interactive
+						experiences. I am always eager to learn new technologies
+						and frameworks. I am passionate about building excellent
+						software that improves the lives of those around me.
+						That is why I am always looking for new opportunities to
+						work with people who share the same passion and vision
+						as me.
+					</div>
+					<div className="self-stretch text-black text-sm md:text-3xl font-semibold font-['sora']">
+						huy.buihoang2301@gmail.com
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 {
-    /* 
+	/* 
                             <div className="self-stretch  gap-4 inline-flex">
                                 <div className="text-black text-5xl font-extrabold font-['sora'] leading-10">
                                     Let’s
--- src/app/pages/welcome.tsx
+++ src/app/pages/welcome.tsx
@@ -1,126 +1,86 @@
+import { url } from "inspector";
 import { Sora } from "next/font/google";
 import React from "react";
 const sora = Sora({ subsets: ["latin"] });
 export default function Welcome() {
-    return (
-        <div className="w-full pt-5 px-6 flex items-center justify-center justify-items-center">
-            <div className="w-full md:w-3/4 h-80 flex-col justify-center items-center gap-14 inline-flex text-center">
-                <div className="self-stretch h-14 flex-col justify-center items-center flex text-3xl md:text-5xl ">
-                    <div className="justify-between items-start gap-4 font-['sora']">
-                        <span className="text-black  font-normal leading-tight justify-between content-between">
-                            Hello I’am{" "}
-                        </span>
-                        <span className="text-black font-extrabold">
-                            Bui Hoang Huy.{" "}
-                        </span>
-                        <span className="text-black  font-normal justify-between content-between">
-                            I'm a{" "}
-                        </span>
-                        <span className="text-black font-extrabold  ">
-                            SE Student{" "}
-                        </span>
-                        <span className="text-black  font-normal justify-between content-between">
-                            Based In{" "}
-                        </span>
-                        <span className="text-black  font-extrabold ">
-                            Vietnam.
-                        </span>
-                    </div>
-                </div>
-                <div className="self-stretch text-zinc-500  font-normal font-['Sora'] leading-6 tracking-tight justify-center items-center text-justify">
-                    I'm Bui Hoang Huy. I'm a CS student based in Vietnam. I love
-                    to code and build things that live on the internet. I
-                    develop exceptional websites and web apps that provide
-                    intuitive, pixel-perfect user interfaces with efficient and
-                    modern backends. I am a fast learner and I am always eager
-                    to learn new technologies and frameworks. I am passionate
-                    about building excellent software that improves the lives of
-                    those around me.
-                </div>
-                <div className="w-full grid grid-cols-2 h-16 py-1 place-items-center justify-center items-center gap-8 md:inline-flex">
-                    <div className="w-14 h-14 p-4 border-2 border-black rounded justify-center items-center gap-2 flex">
-                        <div className="w-5 h-5 justify-center items-center flex">
-                            <div className="w-5 h-5 relative flex-col justify-start items-start flex">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 24 24"
-                                    fill="currentColor"
-                                    className="w-6 h-6"
-                                >
-                                    <path d="M10.5 18.75a.75.75 0 0 0 0 1.5h3a.75.75 0 0 0 0-1.5h-3Z" />
-                                    <path
-                                        fill-rule="evenodd"
-                                        d="M8.625.75A3.375 3.375 0 0 0 5.25 4.125v15.75a3.375 3.375 0 0 0 3.375 3.375h6.75a3.375 3.375 0 0 0 3.375-3.375V4.125A3.375 3.375 0 0 0 15.375.75h-6.75ZM7.5 4.125C7.5 3.504 8.004 3 8.625 3H9.75v.375c0 .621.504 1.125 1.125 1.125h2.25c.621 0 1.125-.504 1.125-1.125V3h1.125c.621 0 1.125.504 1.125 1.125v15.75c0 .621-.504 1.125-1.125 1.125h-6.75A1.125 1.125 0 0 1 7.5 19.875V4.125Z"
-                                        clip-rule="evenodd"
-                                    />
-                                </svg>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="w-14 h-14 p-4 rounded border-2 border-black justify-center items-center gap-2 flex">
-                        <div className="w-5 h-5 justify-center items-center flex">
-                            <div className="w-auto h-auto relative flex-col justify-start items-start flex">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke-width="1.5"
-                                    stroke="currentColor"
-                                    className="w-6 h-6"
-                                >
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        d="m9 9 10.5-3m0 6.553v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 1 1-.99-3.467l2.31-.66a2.25 2.25 0 0 0 1.632-2.163Zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 0 1-.99-3.467l2.31-.66A2.25 2.25 0 0 0 9 15.553Z"
-                                    />
-                                </svg>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="w-14 h-14 p-4 rounded border-2 border-black justify-center items-center gap-2 flex">
-                        <div className="w-5 h-5 justify-center items-center flex">
-                            <div className="w-5 h-5 relative flex-col justify-start items-start flex">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth={1.5}
-                                    stroke="currentColor"
-                                    className="w-6 h-6"
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z"
-                                    />
-                                </svg>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="w-14 h-14 p-4 rounded border-2 border-black justify-center items-center gap-2 flex">
-                        <div className="w-5 h-5 justify-center items-center flex">
-                            <div className="w-5 h-5 px-px justify-center items-center inline-flex">
-                                <div className="w-4 h-5 relative">
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        stroke-width="1.5"
-                                        stroke="currentColor"
-                                        className="w-6 h-6"
-                                    >
-                                        <path
-                                            stroke-linecap="round"
-                                            stroke-linejoin="round"
-                                            d="M3.75 3v11.25A2.25 2.25 0 0 0 6 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0 1 18 16.5h-2.25m-7.5 0h7.5m-7.5 0-1 3m8.5-3 1 3m0 0 .5 1.5m-.5-1.5h-9.5m0 0-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6"
-                                        />
-                                    </svg>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+	const contact_icons = [
+		{
+			name: "Phone",
+			src: "https://cdn.jsdelivr.net/npm/tabler-icons@1.35.0/icons/phone.svg",
+			url: "0326540994",
+		},
+		{
+			name: "Mail",
+			src: "https://cdn.jsdelivr.net/npm/tabler-icons@1.35.0/icons/mail.svg",
+			url: "huy.buihoang2301@gmai.com",
+		},
+		{
+			name: "Github",
+			src: "https://cdn.jsdelivr.net/npm/tabler-icons@1.35.0/icons/brand-github.svg",
+			url: "https://github.com/towh2301/",
+		},
+	];
+
+	return (
+		<div className="w-full pt-5 px-6 flex items-center justify-center justify-items-center">
+			<div className="w-full md:w-3/4 h-80 flex-col justify-center items-center gap-14 inline-flex text-center">
+				<div className="self-stretch h-14 flex-col justify-center items-center flex text-3xl md:text-5xl ">
+					<div className="justify-between items-start gap-4 font-['sora']">
+						<span className="text-black  font-normal leading-tight justify-between content-between">
+							Hello I’am{" "}
+						</span>
+						<span className="text-black font-extrabold">
+							Bui Hoang Huy.{" "}
+						</span>
+						<span className="text-black  font-normal justify-between content-between">
+							I'm a{" "}
+						</span>
+						<span className="text-black font-extrabold  ">
+							SE Student{" "}
+						</span>
+						<span className="text-black  font-normal justify-between content-between">
+							Based In{" "}
+						</span>
+						<span className="text-black  font-extrabold ">
+							Vietnam.
+						</span>
+					</div>
+				</div>
+				<div className="self-stretch text-zinc-500  font-normal font-['Sora'] leading-6 tracking-tight justify-center items-center text-justify">
+					I'm Bui Hoang Huy. I'm a CS student based in Vietnam. I love
+					to code and build things that live on the internet. I
+					develop exceptional websites and web apps that provide
+					intuitive, pixel-perfect user interfaces with efficient and
+					modern backends. I am a fast learner and I am always eager
+					to learn new technologies and frameworks. I am passionate
+					about building excellent software that improves the lives of
+					those around me.
+				</div>
+				<div className="w-full grid grid-cols-2 h-16 py-1 place-items-center justify-center items-center gap-8 md:inline-flex">
+					{contact_icons.map((icon, index) => (
+						<div
+							className="w-14 h-14 p-4 border-2 border-black rounded justify-center items-center gap-2 flex"
+							key={index}
+							onClick={() => {
+								if (icon.name !== "Github") {
+								} else {
+									location.href = icon.url;
+								}
+							}}
+						>
+							<div className="w-5 h-5 justify-center items-center flex">
+								<div className="w-5 h-5 relative flex-col justify-start items-start flex">
+									<img
+										src={icon.src}
+										alt={icon.name}
+										className="w-6 h-6 object-contain"
+									/>
+								</div>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 }
